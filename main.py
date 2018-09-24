af8287e9f59b2c494f6e6dd519d452477515ea33
gshpychka <glibshpychka@gmail.com>
2018-09-24 17:15:42 +0300

    Added distributions with age.

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
 import matplotlib.ticker as ticker
 import seaborn as sns
+sns.set()
 import datetime as dt
 import os
 
@@ -21,6 +22,7 @@ def load_labels_into_df():
 
 
 labels = load_labels_into_df()
+labels = labels[labels['Patient Age'].isin(range(18, 95))]
 labels = labels[['Image Index', 'Finding Labels', 'Patient ID', 'Patient Age', 'Patient Gender']]
 
 #%%
@@ -63,12 +65,12 @@ ax2 = plt.subplot2grid((8,1), (7,0), rowspan=1)
 
 data1 = pd.melt(labels,
              id_vars=['Patient Gender'],
-             value_vars = list(findings_list),
-             var_name = 'Category',
-             value_name = 'Count')
+             value_vars=findings_list,
+             var_name='Finding',
+             value_name='Count')
 data1 = data1.loc[data1.Count>0]
 
-g=sns.countplot(y='Category',hue='Patient Gender',data=data1, ax=ax1, order = data1['Category'].value_counts().index)
+g=sns.countplot(y='Finding',hue='Patient Gender',data=data1, ax=ax1, order = data1['Finding'].value_counts().index)
 ax1.set( ylabel="",xlabel="")
 ax1.legend(fontsize=20)
 ax1.set_title('Findings per gender',fontsize=18);
@@ -77,11 +79,11 @@ labels['No Finding']=labels['Finding Labels'].apply(lambda x: 1 if 'No Finding' 
 
 data2 = pd.melt(labels,
              id_vars=['Patient Gender'],
-             value_vars = list(['No Finding']),
-             var_name = 'Category',
-             value_name = 'Count')
+             value_vars=['No Finding'],
+             var_name='Finding',
+             value_name='Count')
 data2 = data2.loc[data2.Count>0]
-g=sns.countplot(y='Category',hue='Patient Gender',data=data2,ax=ax2)
+g=sns.countplot(y='Finding',hue='Patient Gender',data=data2,ax=ax2)
 ax2.set( ylabel="",xlabel="Count")
 ax2.legend('')
 plt.subplots_adjust(hspace=.5)
@@ -89,19 +91,34 @@ plt.subplots_adjust(hspace=.5)
 plt.show()
 
 #%%
-
+# Gender ditributions per finding
 plt.figure(figsize=(15,10))
 ax1 = plt.subplot()
-
-counts = data1.groupby(['Category', 'Patient Gender']).count()
-
+counts = data1.groupby(['Finding', 'Patient Gender']).count()
 counts = counts.unstack(level=1)
 counts.columns = counts.columns.droplevel(level=0)
-
-#%%
-
 percentages = counts.apply(lambda x: [x['M'] * 100 / x.sum(), 100 - (x['M'] * 100 / x.sum())], axis=1, result_type='broadcast')
+percentages.plot(kind='barh', stacked=True, title='Findings distribution by gender')
+
+
+plt.show()
 
 #%%
-percentages.plot(kind='barh', stacked=True, title='Findings distribution by gender')
-plt.show()
+# Finding incidence distribution by age
+age_data = pd.melt(labels,
+                   id_vars=['Patient Age'],
+                   value_vars=findings_list + ['No Finding'],
+                   var_name='Finding',
+                   value_name='Count')
+age_data = age_data.loc[age_data.Count>0]
+
+#%%
+
+for label in findings_list + ['No Finding']:
+    plt.figure(figsize=(15, 10))
+    ax1 = plt.subplot()
+    sns.distplot(age_data[age_data['Finding'] == label]['Patient Age'], ax=ax1, bins=range(18, 95)[0::2])
+    ax1.set(xlabel='Age', ylabel='Incidence')
+    ax1.set_title(label, fontsize=18)
+    #plt.show()
+    plt.savefig('plots/' + label + '_age_distribution_plot.png', format='png')
