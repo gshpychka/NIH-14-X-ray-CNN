547ab53df1758b54e0f4f6874e012fa9437c12d0
gshpychka <glibshpychka@gmail.com>
2018-09-25 16:28:04 +0300

    Refactoring, bugfixes

--- preprocessing.py
+++ preprocessing.py
@@ -46,68 +46,12 @@ def preprocess_labels():
     labels['Any Finding'] = labels['Finding Labels'].apply(lambda x: 0 if 'No Finding' in x else 1)
     return labels
 
+labels = preprocess_labels()
 
-def preprocess_dataset_in_chunks(dataset_old = h5py.File('data/data.h5', 'r')):
-
-    labels = pd.read_csv('labels.csv')
-    chunk = 1000;
-
-    with h5py.File('data_new.h5', 'w') as dataset_new:
-        start = dt.datetime.now()
-
-        label_group = dataset_new.create_group('labels')
-        # storing numerical labels
-        numerical_labels = ['Patient ID', 'Patient Age']
-        for label in numerical_labels:
-            label_group.create_dataset(
-                name=label,
-                data=labels[label],
-                shape=labels[label].shape,
-            )
-        # storing string labels
-        string_labels = ['Image Index', 'Finding Labels', 'Patient Gender']
-        for label in string_labels:
-            string_dt = h5py.special_dtype(vlen=str)
-            data = np.asarray(labels[label], dtype=object)
-            label_group.create_dataset(label, data=data, dtype=string_dt)
-
-
-        Xset = dataset_new.create_dataset(
-            name='X',
-            data=None,
-            shape=(0,224,224),
-            maxshape=(None, 224, 224),
-            chunks=None,
-            compression='gzip',
-            compression_opts=9
-        )
-        for k in range(0, int(len(labels) / chunk)):
-            X = np.empty((chunk, 224, 224))
-            for i in range(0, chunk):
-                img_num = chunk * k + i
-                X[i] = dataset_old[labels['Image Index'][img_num]]
-                print("Transferred image %d into array." % img_num)
-            Xset.resize(Xset.shape[0] + chunk, axis=0)
-            Xset[-chunk:] = X
-    end = dt.datetime.now()
-    print("Done, in %f minutes, yo." % ((end - start) / 60))
-
-
-def preprocess():
-    start = dt.datetime.now()
-    PATH = os.path.abspath('data')
-    labels = pd.read_csv('labels.csv')
-    # filenames
-    images = labels['Image Index']
-    print('There are %d images in the folder.' % len(images))
-
-    width = 224
-    height = width
-    hf = h5py.File('data.h5', 'w')
-
-    label_group = hf.create_group('labels')
+def store_labels_hdf5(file = h5py.File(os.path.abspath('data/data_new.h5'), 'w')):
+    label_group = file.create_group('labels')
     # storing numerical labels
-    numerical_labels = ['Patient ID', 'Patient Age']
+    numerical_labels = ['Patient ID', 'Patient Age', 'No Finding', 'Any Finding'] + get_findings_list()
     for label in numerical_labels:
         label_group.create_dataset(
             name=label,
@@ -115,20 +59,60 @@ def preprocess():
             shape=labels[label].shape,
         )
     # storing string labels
-    string_labels = ['Image Index', 'Finding Labels', 'Patient Gender']
+    string_labels = ['Image Index', 'Patient Gender']
     for label in string_labels:
         string_dt = h5py.special_dtype(vlen=str)
         data = np.asarray(labels[label], dtype=object)
         label_group.create_dataset(label, data=data, dtype=string_dt)
 
+    file.close()
 
+
+def preprocess_images_into_dataset_in_chunks(file_old = h5py.File(os.path.abspath('data/data.h5'), 'r'), file_new=h5py.File(os.path.abspath('data/data_new.h5'), 'w')):
+    chunk = 1000;
+    start = dt.datetime.now()
+
+    Xset = file_new.create_dataset(
+        name='X',
+        data=None,
+        shape=(0,224,224),
+        maxshape=(None, 224, 224),
+        chunks=None,
+        compression='gzip',
+        compression_opts=9
+    )
+
+    for k in range(0, int(len(labels) / chunk)):
+        X = np.empty((chunk, 224, 224))
+        for i in range(0, chunk):
+            img_num = chunk * k + i
+            X[i] = file_old[labels['Image Index'][img_num]]
+            print("Transferred image %d into array." % img_num)
+        Xset.resize(Xset.shape[0] + chunk, axis=0)
+        Xset[-chunk:] = X
+    end = dt.datetime.now()
+    print("Done, in %s, yo." % (end - start))
+    file_old.close()
+    file_new.close()
+
+
+def preprocess_images_into_separate_datasets(file = h5py.File(os.path.abspath('data/data.h5'), 'w')):
+    start = dt.datetime.now()
+    PATH = os.path.abspath('../NIHChestXrayDataset')
+
+    # filenames
+    images = labels['Image Index']
+    print('There are %d images in the dataset.' % len(images))
+
+    width = 224
+    height = width
 
     for i, img in enumerate(images):
         # images
         image = cv2.imread(os.path.join(PATH, img))
         image = cv2.resize(image, (width, height), interpolation=cv2.INTER_CUBIC)
         image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        Xset = hf.create_dataset(
+        Xset = file.create_dataset(
             name=img,
             data=image,
             shape=(width, height),
@@ -138,9 +122,11 @@ def preprocess():
         )
         print('Processed image %d' % i)
     end = dt.datetime.now()
-    print('Processed %d images in %f minutes' % (i, ((end - start) / 60)))
+    print('Processed %d images in %s' % (i, (end - start)))
 
-    return hf
+    file.close()
+
+
 
 
 def test(filename='data.h5', num=5):
