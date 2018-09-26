be0ae3c2e9197584d06a0f8eb0fc370e3e77c7c7
gshpychka <glibshpychka@gmail.com>
2018-09-26 13:07:31 +0300

    Added MobileNet implementation from keras.applications, modified to work with 1 channel.

--- model.py
+++ model.py
@@ -1,1 +1,53 @@
-import tensorflow.keras
+
+from keras_mobilenet_helper import _conv_block, _depthwise_conv_block
+from tensorflow.python.keras.layers import GlobalAveragePooling2D
+from tensorflow.python.keras.layers import GlobalMaxPooling2D
+from tensorflow.python.keras.layers import Input, Dropout, Dense
+from tensorflow.python.keras.models import Model
+
+
+def get_model(alpha=1, depth_multiplier=1, pooling='max'):
+
+    img_input = Input(shape=(224, 224, 1))
+
+    x = _conv_block(img_input, 32, alpha, strides=(2, 2))
+    x = _depthwise_conv_block(x, 64, alpha, depth_multiplier, block_id=1)
+
+    x = _depthwise_conv_block(
+        x, 128, alpha, depth_multiplier, strides=(2, 2), block_id=2)
+    x = _depthwise_conv_block(x, 128, alpha, depth_multiplier, block_id=3)
+
+    x = _depthwise_conv_block(
+        x, 256, alpha, depth_multiplier, strides=(2, 2), block_id=4)
+    x = _depthwise_conv_block(x, 256, alpha, depth_multiplier, block_id=5)
+
+    x = _depthwise_conv_block(
+        x, 512, alpha, depth_multiplier, strides=(2, 2), block_id=6)
+    x = _depthwise_conv_block(x, 512, alpha, depth_multiplier, block_id=7)
+    x = _depthwise_conv_block(x, 512, alpha, depth_multiplier, block_id=8)
+    x = _depthwise_conv_block(x, 512, alpha, depth_multiplier, block_id=9)
+    x = _depthwise_conv_block(x, 512, alpha, depth_multiplier, block_id=10)
+    x = _depthwise_conv_block(x, 512, alpha, depth_multiplier, block_id=11)
+
+    x = _depthwise_conv_block(
+        x, 1024, alpha, depth_multiplier, strides=(2, 2), block_id=12)
+    x = _depthwise_conv_block(x, 1024, alpha, depth_multiplier, block_id=13)
+
+    if pooling == 'avg':
+        x = GlobalAveragePooling2D()(x)
+    elif pooling == 'max':
+        x = GlobalMaxPooling2D()(x)
+
+    x = Dropout(0.5)(x)
+    x = Dense(512)(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1, activation='sigmoid')(x)
+
+
+    # Create model.
+    model = Model(img_input, x, name='mobilenet_%0.2f_%s' % (alpha, 224))
+
+    model.compile(optimizer='adam', loss='binary_crossentropy',
+                  metrics=['binary_accuracy', 'mae'])
+
+    return model
--- training.py
+++ training.py
@@ -3,33 +3,36 @@ from tensorflow.keras.utils import HDF5Matrix
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import pandas as pd
 import numpy as np
+from model import get_model
 
 datapath = 'data/data_new.h5'
 
-
-datagen = ImageDataGenerator(
-    featurewise_center=True,
-    featurewise_std_normalization=True,
-    width_shift_range=0.1,
-    height_shift_range=0.1,
-    rotation_range=3
-)
-
-X_representative_set = HDF5Matrix(datapath, 'X', end=5000)
-
-print("Fitting mean and std on a representative set...")
-datagen.fit(X_representative_set)
-
-def load_data(train_start, n_train, test_start, n_test):
+def load_data(train_start=0, n_train=50000, test_start=80000, n_test=1000):
     X_train = HDF5Matrix(datapath, 'X', train_start, train_start+n_train)
-    y_train = HDF5Matrix(datapath, 'Any Finding', train_start, train_start+n_train)
+    y_train = HDF5Matrix(datapath, 'labels/Any Finding', train_start, train_start+n_train)
     X_test = HDF5Matrix(datapath, 'X', test_start, test_start+n_test)
-    y_test = HDF5Matrix(datapath, 'Any Finding', test_start, test_start+n_test)
+    y_test = HDF5Matrix(datapath, 'labels/Any Finding', test_start, test_start+n_test)
     return X_train, y_train, X_test, y_test
 
-def train_model(model, X_train, y_train, datagen, epochs=1, batch_size=32):
-    for e in range(epochs):
-        print('Currently in epoch %d out of %d' % (e+1, epochs))
-        for X_batch, y_batch in datagen.flow(X_train, y_train, batch_size=batch_size):
-            loss = model.train(X_batch, y_batch)
-            print("Current loss: %d" % loss)
+
+def train_model(model, X_train, y_train, X_test, y_test, datagen, epochs=1, batch_size=32):
+    model.fit_generator(datagen.flow(X_train, y_train, shuffle=False, batch_size=batch_size),
+                        steps_per_epoch=len(X_train) / batch_size,
+                        epochs=epochs,
+                        validation_data=(X_test,y_test))
+
+if __name__ == '__main__':
+    model = get_model()
+    datagen = ImageDataGenerator(
+        featurewise_center=True,
+        featurewise_std_normalization=True,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        rotation_range=3
+    )
+    X_representative_set = HDF5Matrix(datapath, 'X', end=5000)
+    print("Fitting mean and std on a representative set...")
+    datagen.fit(X_representative_set)
+
+    X_train, y_train, X_test, y_test = load_data()
+    train_model(model, X_train, y_train, X_test, y_test, datagen, batch_size=5)
